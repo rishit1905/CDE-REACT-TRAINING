29e653af8cc8f100a42b58f086ecb73b2cad827c
844236 <844236@cognizant.com>
2020-08-04 01:06:23 +0530

    Assignments(Complete) and Application(Updated)

---
+++ Assignments/A3-PRODUCT INVENTORY SYSTEM.mm
@@ -0,0 +1,123 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="PRODUCT INVENTORY SYSTEM" FOLDED="false" ID="ID_1025351136" CREATED="1596461918141" MODIFIED="1596462805768" BACKGROUND_COLOR="#ffffff"><hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#000000ff,#ff0033ff,#009933ff,#3333ffff,#ff6600ff,#cc00ccff,#ffbf00ff,#00ff99ff,#0099ffff,#996600ff,#000000ff,#cc0066ff,#33ff00ff,#ff9999ff,#0000ccff,#cccc00ff,#0099ccff,#006600ff,#ff00ccff,#00cc00ff,#0066ccff,#00ffffff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt" TEXT_SHORTENED="true">
+<font SIZE="24"/>
+<richcontent TYPE="DETAILS" LOCALIZED_HTML="styles_background_html"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<font SIZE="9"/>
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="bubble" SHAPE_VERTICAL_MARGIN="0.0 pt" TEXT_ALIGN="CENTER" MAX_WIDTH="120.0 pt" MIN_WIDTH="120.0 pt">
+<font NAME="Arial" SIZE="9" BOLD="true" ITALIC="false"/>
+<edge STYLE="bezier" WIDTH="3"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details">
+<font SIZE="11" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes" COLOR="#000000" BACKGROUND_COLOR="#ffffff">
+<font SIZE="9" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT">
+<font BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<font SIZE="9"/>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+<edge COLOR="#0000cc"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<font SIZE="9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" UNIFORM_SHAPE="true" MAX_WIDTH="120.0 pt" MIN_WIDTH="120.0 pt">
+<font SIZE="24" ITALIC="true"/>
+<edge STYLE="bezier" WIDTH="3"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font SIZE="12" BOLD="true" ITALIC="false"/>
+<hook NAME="AutomaticEdgeColor" COUNTER="0" RULE="FOR_COLUMNS"/>
+<node TEXT="Login" POSITION="right" ID="ID_1103167009" CREATED="1596462143541" MODIFIED="1596474340553" HGAP_QUANTITY="14.749999977648256 pt" VSHIFT_QUANTITY="-1.4999999552965164 pt" BACKGROUND_COLOR="#ff6666">
+<font NAME="Copperplate Gothic Bold" SIZE="10" BOLD="false"/>
+<node TEXT="Signup" ID="ID_1545833588" CREATED="1596462230728" MODIFIED="1596474340553" HGAP_QUANTITY="16.999999910593036 pt" VSHIFT_QUANTITY="35.24999894946817 pt" BACKGROUND_COLOR="#3399ff">
+<font NAME="Copperplate Gothic Bold" SIZE="10" BOLD="false"/>
+</node>
+<node TEXT="Header &amp; Footer" ID="ID_1776895580" CREATED="1596462960633" MODIFIED="1596474337498" HGAP_QUANTITY="16.999999910593036 pt" VSHIFT_QUANTITY="8.249999754130847 pt" BACKGROUND_COLOR="#3399ff">
+<font NAME="Copperplate Gothic Bold" SIZE="10" BOLD="false"/>
+<node TEXT="Dashboard" ID="ID_1768151406" CREATED="1596463189983" MODIFIED="1596463442795" BACKGROUND_COLOR="#00cc33">
+<font NAME="Copperplate Gothic Bold" SIZE="10" BOLD="false"/>
+<node TEXT="Welcome User" ID="ID_997764809" CREATED="1596463448385" MODIFIED="1596464405262" BACKGROUND_COLOR="#999900">
+<font NAME="Copperplate Gothic Bold" SIZE="10" BOLD="false"/>
+<node TEXT="Signout" ID="ID_847139345" CREATED="1596474472810" MODIFIED="1596482316320">
+<font NAME="Copperplate Gothic Bold" SIZE="10" BOLD="false"/>
+</node>
+<node TEXT="View Profile" ID="ID_1526110475" CREATED="1596474494107" MODIFIED="1596482330437">
+<font NAME="Copperplate Gothic Bold" SIZE="10" BOLD="false"/>
+</node>
+<node TEXT="Account Settings" ID="ID_184027061" CREATED="1596474535538" MODIFIED="1596482344906">
+<font NAME="Copperplate Gothic Bold" SIZE="10" BOLD="false"/>
+</node>
+</node>
+<node TEXT="Profile Image" ID="ID_676008074" CREATED="1596463481588" MODIFIED="1596464160670" BACKGROUND_COLOR="#999900">
+<font NAME="Copperplate Gothic Bold" SIZE="10" BOLD="false"/>
+</node>
+<node TEXT="Search" ID="ID_882587503" CREATED="1596463525584" MODIFIED="1596464379302" BACKGROUND_COLOR="#999900">
+<font NAME="Copperplate Gothic Bold" SIZE="10" BOLD="false"/>
+</node>
+<node TEXT="" ID="ID_590628696" CREATED="1596474143800" MODIFIED="1596474143802">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="Featuring" ID="ID_1499627716" CREATED="1596463567866" MODIFIED="1596464166859" BACKGROUND_COLOR="#999900">
+<font NAME="Copperplate Gothic Bold" SIZE="10" BOLD="false"/>
+</node>
+<node TEXT="Category" ID="ID_1263067550" CREATED="1596463659785" MODIFIED="1596464174139" BACKGROUND_COLOR="#999900">
+<font NAME="Copperplate Gothic Bold" SIZE="10" BOLD="false"/>
+</node>
+<node TEXT="Sub-Category" ID="ID_1283165522" CREATED="1596463740244" MODIFIED="1596464259058" BACKGROUND_COLOR="#999900">
+<font NAME="Copperplate Gothic Bold" SIZE="10" BOLD="false"/>
+</node>
+<node TEXT="Monthly Statistics" ID="ID_308444255" CREATED="1596463756593" MODIFIED="1596464179363" BACKGROUND_COLOR="#999900">
+<font NAME="Copperplate Gothic Bold" SIZE="10" BOLD="false"/>
+</node>
+<node TEXT="Vendor" ID="ID_1257457361" CREATED="1596463765506" MODIFIED="1596464183950" BACKGROUND_COLOR="#999900">
+<font NAME="Copperplate Gothic Bold" SIZE="10" BOLD="false"/>
+</node>
+<node TEXT="Inventory Management" ID="ID_1411056181" CREATED="1596463824029" MODIFIED="1596474114861" BACKGROUND_COLOR="#999900">
+<font NAME="Copperplate Gothic Bold" SIZE="10" BOLD="false"/>
+</node>
+<node TEXT="" ID="ID_1818293637" CREATED="1596474143799" MODIFIED="1596474367486">
+<hook NAME="SummaryNode"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="Inventory" ID="ID_298266408" CREATED="1596474143803" MODIFIED="1596474418804">
+<font NAME="Copperplate Gothic Bold" SIZE="10" BOLD="false"/>
+<node TEXT="Product" ID="ID_1226850900" CREATED="1596474180473" MODIFIED="1596474445455" BACKGROUND_COLOR="#00cccc" HGAP_QUANTITY="14.749999977648258 pt" VSHIFT_QUANTITY="-13.49999959766866 pt">
+<font NAME="Copperplate Gothic Bold" SIZE="10" BOLD="false"/>
+<node TEXT="Product Description" ID="ID_630676733" CREATED="1596474247081" MODIFIED="1596474414154" BACKGROUND_COLOR="#cc00cc">
+<font NAME="Copperplate Gothic Bold" SIZE="10" BOLD="false"/>
+</node>
+<node TEXT="Update" ID="ID_1192317983" CREATED="1596474269692" MODIFIED="1596474409498" BACKGROUND_COLOR="#cc00cc">
+<font NAME="Copperplate Gothic Bold" SIZE="10" BOLD="false"/>
+</node>
+<node TEXT="Delete" ID="ID_141615056" CREATED="1596474277558" MODIFIED="1596474403581" BACKGROUND_COLOR="#cc00cc">
+<font NAME="Copperplate Gothic Bold" SIZE="10" BOLD="false"/>
+</node>
+</node>
+<node TEXT="ADD PRODUCT" ID="ID_1766816303" CREATED="1596474189858" MODIFIED="1596474449085" BACKGROUND_COLOR="#00cccc">
+<font NAME="Copperplate Gothic Bold" SIZE="10" BOLD="false"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
