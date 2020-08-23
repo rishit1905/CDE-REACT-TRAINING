6327e9d0262fafcba25d0216f3e5fbb05a1228bf
844236 <844236@cognizant.com>
2020-08-23 22:18:19 +0530

    React Native

--- react-native-product-inventory/components/productdetail.js
+++ react-native-product-inventory/components/productdetail.js
@@ -1,22 +1,44 @@
 import React, { useState, useEffect } from 'react';
-import { ScrollView, View, Text } from 'react-native';
+import { ScrollView, View, Text, Image } from 'react-native';
 import { Card } from 'react-native-elements';
 
-export default function ProductDetail({ route, navigation }) {
+export default function ProductDetail({ route }) {
+
+    const { item } = route.params
+
+    const [imageURL, setImageURL] = useState("")
+    const [name, setName] = useState("")
+    const [brand, setBrand] = useState("")
+    const [description, setDescription] = useState("")
+    const [category, setCategory] = useState("Mobiles")
+    const [price, setPrice] = useState(0)
+    const [stock, setStock] = useState(0)
+
+    useEffect(() => {
+        setImageURL(item.imageURL)
+        setName(item.name)
+        setBrand(item.brand)
+        setDescription(item.description)
+        setCategory(item.category)
+        setPrice(item.price)
+        setStock(item.stock)
+    })
 
     return (
         <View>
             <Card>
-                <Text>Product Name: {route.params.name}</Text>
-                <Text>Brand: {route.params.brand}</Text>
-                <Text>Description: {route.params.description}</Text>
-                <Text>Category: {route.params.category}</Text>
-                <Text>Price: {route.params.price}</Text>
-                <Text>Stock: {route.params.stock}</Text>
-            </Card>
-            {/* <Image
+                <Image
+                    source={{ uri: imageURL }}
                     style={{ width: 200, height: 200 }}
-                >{imageURL}</Image> */}
+                />
+                <Text>Product Name: {name}</Text>
+                <Text>Brand: {brand}</Text>
+                <Text>Description: {description}</Text>
+                <Text>Category: {category}</Text>
+                <Text>Price: {price}</Text>
+                <Text>Stock: {stock}</Text>
+            </Card>
+
 
         </View>
     )
